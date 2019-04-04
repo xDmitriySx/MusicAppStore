21306bbb9dc5afde8519f2d5a67ab4269d1790f0
xDmitriySx <lexman991@gmail.com>
2019-04-04 04:28:24 +0300

    Update MusicCardContainer.js

--- src/containers/MusicCardContainer.js
+++ src/containers/MusicCardContainer.js
@@ -3,16 +3,14 @@ import MusicCardComponent from '../components/MusicCardComponent';
 import * as cartAtions from '../actions/cart_action'
 import { bindActionCreators } from 'redux';
 
-
 const mapStateToProps = ({ cart_reducer }, {id}) => ({
     addedCount: cart_reducer.cart_reducer_items.reduce((count, mus) => count + (mus.id === id ? 1 : 0), 0), 
-    ind: Date.now()
+    ind: Date.now() /* creating identification keys for removing certain elements. ( not  ) */
 });
 
 const mapDispatchToProps = dispatch => ({
     ...bindActionCreators(cartAtions, dispatch)
 })
-
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(MusicCardComponent);
